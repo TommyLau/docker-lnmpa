8b82071d14ca3122375b8722f770c83d2522362f
Tommy Lau <tommy@gen-new.com>
2015-02-13 11:13:33 +0800

    Added output in script

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# Using under boot2docker, fix rights
 if [ ! -z "$BOOT2DOCKER" ] && [ -z `grep docker /etc/apache2/apache2.conf` ]; then
+	# Using under boot2docker, fix rights
+	echo "Using 'docker/staff' for Apache2 in boot2docker . . ."
+
 	# Add a user for Apache2
 	useradd -u 1000 -g 50 docker
 
